c5c669484c86ff0e46c4ebb65c1e63ae04e05619
kazuki takahashi <tkhsh.schoen@gmail.com>
2018-10-03 13:50:26 +0900

    dockerのオプションを追加できるようにした

--- run_nvidia_docker.sh
+++ run_nvidia_docker.sh
@@ -13,10 +13,17 @@ devs=${devs#,}
 
 echo "Using GPUs:"$devs
 
+read -e -p "Enter docker options:" OPTIONS
+
+OPTIONS="$(eval "echo $OPTIONS")"
+
+echo $OPTIONS
+
 read -e -p "Enter command:" COMMAND
 
 NV_GPU=$devs nvidia-docker run -it --rm \
   --env=QT_X11_NO_MITSHM=1 \
   --env=DISPLAY=$DISPLAY \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  $OPTIONS \
   $1 $COMMAND
